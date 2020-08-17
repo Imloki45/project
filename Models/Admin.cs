ad962b2248d388dea2d0005609585b000bd47736
suryateja503 <67820768+suryateja503@users.noreply.github.com>
2020-08-17 19:56:49 +0530

    Add files via upload

---
+++ Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCSProject.Models
+{
+    public class Admin
+    {
+        public int AdmId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+
+        public string LoginErrorMessage { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TCSProject.Models
+{
+    public class Employee
+    {
+
+        
+        public int EmpId
+        {
+            get; set; 
+        }
+        
+        public string FirstName { get; set; }
+
+        
+        public string LastName { get; set; }
+
+        
+        public string Password { get; set; }
+
+        
+        public string  Email { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime  DOB { get; set; }
+
+
+        public int ProjectId { get; set; }
+        public int WONNumber { get; set; }
+
+        public string ProjectDetails { get; set; }
+        public string LoginErrorMessage { get; set; }
+    }
+}
